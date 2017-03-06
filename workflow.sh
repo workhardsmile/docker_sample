4eb54fd620203ad622cc302c70eaadaceae2304f
Frank Wu <gang.wu@istuary.com>
2017-03-06 22:42:30 +0800

    docker-compose example

---
+++ workflow.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#docker-compose build
+mkdir -p $HOME/opt/htdocs/ && cp -f hellotest.php $HOME/opt/htdocs/index.php
+[ ! -d "$HOME/opt/htdocs/covision" ] && git clone --no-checkout "git@10.0.10.120:OnlineTestSystem/covision.git" $HOME/opt/htdocs/covision
+#cd $HOME/opt/htdocs/covision&&git pull
+#git checkout $(git tag|sort -r|head -n 1)
+cd - && docker-compose up -d &
+docker exec -it $(docker ps|grep php|awk '{print $1}') "/usr/local/bin/composer install"
