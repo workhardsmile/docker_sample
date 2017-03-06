e3cc4c8e47c7e60fa69c1d89953acb47306291d1
Frank Wu <gang.wu@istuary.com>
2017-03-06 22:51:23 +0800

    update php dockerfile

--- workflow.sh
+++ workflow.sh
@@ -4,5 +4,5 @@ mkdir -p $HOME/opt/htdocs/ && cp -f hellotest.php $HOME/opt/htdocs/index.php
 [ ! -d "$HOME/opt/htdocs/covision" ] && git clone --no-checkout "git@10.0.10.120:OnlineTestSystem/covision.git" $HOME/opt/htdocs/covision
 #cd $HOME/opt/htdocs/covision&&git pull
 #git checkout $(git tag|sort -r|head -n 1)
-cd - && docker-compose up -d &
-docker exec -it $(docker ps|grep php|awk '{print $1}') "/usr/local/bin/composer install"
+cd - && docker-compose up -d
+docker exec -it $(docker ps|grep php|awk '{print $1}') /usr/local/bin/composer install
