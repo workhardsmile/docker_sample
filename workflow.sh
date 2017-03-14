ae8696f17b24250f6cadbbd7f2d11a9803a0ddb7
Frank Wu <gang.wu@istuary.com>
2017-03-14 16:48:25 +0800

    update docker files

--- workflow.sh
+++ workflow.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+registration_name="192.168.29.158:5000/lnmp-php-nginx-alpine"
+
+docker stop $(docker ps -a -q)|xargs docker rm ; docker rmi $(docker images | grep "$registration_name" | awk '{print $3}') -f
+#docker-compose stop
 #docker-compose build
-mkdir -p $HOME/opt/htdocs/ && cp -f hellotest.php $HOME/opt/htdocs/index.php
-[ ! -d "$HOME/opt/htdocs/covision" ] && git clone --no-checkout "git@10.0.10.120:OnlineTestSystem/covision.git" $HOME/opt/htdocs/covision
-#cd $HOME/opt/htdocs/covision&&git pull
-#git checkout $(git tag|sort -r|head -n 1)
-cd - && docker-compose up -d
-docker exec -it $(docker ps|grep php|awk '{print $1}') /usr/local/bin/composer install
+#docker tag & docker push
+docker-compose -f docker-compose-registry.yml up -d mysql mongo
+sleep 30s
+docker-compose -f docker-compose-registry.yml up -d php_nginx
+
+container_id=$(docker ps|grep "$registration_name"|awk '{print $1}')
+docker exec -it --user www-data $container_id git pull
+docker exec -it --user www-data $container_id composer install
+docker exec -it --user www-data $container_id php artisan migrate:install
+docker exec -it --user www-data $container_id php artisan migrate
+docker exec -it --user www-data $container_id php artisan db:seed
+docker commit $container_id $registration_name:latest
+docker push $registration_name:latest
